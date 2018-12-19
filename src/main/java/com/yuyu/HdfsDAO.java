3a0b73b47abf0e7b1286fe44cedd2b9ba4b1f0cf
yu <jiayu1922@163.com>
2018-12-19 12:54:52 +0800

    some instruction about hdfs

---
+++ src/main/java/com/yuyu/HdfsDAO.java
@@ -0,0 +1,81 @@
+package com.yuyu;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobConf;
+
+public class HdfsDAO {
+
+    //HDFS访问地址
+    private static final String HDFS = "hdfs://192.168.126.130:9000/";
+
+    public HdfsDAO(Configuration conf) {
+        this(HDFS, conf);
+    }
+
+    public HdfsDAO(String hdfs, Configuration conf) {
+        this.hdfsPath = hdfs;
+        this.conf = conf;
+    }
+
+    //hdfs路径
+    private String hdfsPath;
+    //Hadoop系统配置
+    private Configuration conf;
+
+    //启动函数
+    public static void main(String[] args) throws IOException {
+        JobConf conf = config();
+        HdfsDAO hdfs = new HdfsDAO(conf);
+        hdfs.ls("/user");
+        hdfs.mkdirs("/tryToMkdirs");
+        hdfs.rmr("/tryToMkdirs");
+    }        
+    
+    //加载Hadoop配置文件
+    public static JobConf config(){
+        JobConf conf = new JobConf(HdfsDAO.class);
+        conf.setJobName("HdfsDAO");
+        conf.addResource("classpath:/hadoop/core-site.xml");
+        conf.addResource("classpath:/hadoop/hdfs-site.xml");
+        conf.addResource("classpath:/hadoop/mapred-site.xml");
+        return conf;
+    }
+    
+    public void ls(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        FileStatus[] list = fs.listStatus(path);
+        System.out.println("ls: " + folder);
+        System.out.println("==========================================================");
+        for (FileStatus f : list) {
+            System.out.printf("name: %s, folder: %s, size: %d\n", f.getPath(), f.isDir(), f.getLen());
+        }
+        System.out.println("==========================================================");
+        fs.close();
+    }
+    
+    public void mkdirs(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);//找到hdfs的URI路径
+        if (!fs.exists(path)) {
+            fs.mkdirs(path);
+            System.out.println("Create: " + folder);
+        }
+        fs.close();
+    }
+    
+    public void rmr(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.deleteOnExit(path);
+        System.out.println("Delete: " + folder);
+        fs.close();
+    }
+    
+}
